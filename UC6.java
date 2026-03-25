e2cf6bc66885e9561f0969627ae8412b4462eb2c
jk9358 <jk9358@srmist.edu.in>
2026-03-25 08:53:12 +0530

    UC6: Use enhanced for-loop and substring to format names

--- UC6.java
+++ UC6.java
@@ -5,16 +5,11 @@ public class UC6 {
         } else {
             StringBuilder nameBuilder = new StringBuilder();
             
-            // Enhanced for-loop to add names and a delimiter
             for (String name : args) {
                 nameBuilder.append(name).append(", ");
             }
-
-            // Convert to String
             String allNames = nameBuilder.toString();
 
-            // Use substring to remove the trailing ", " (last 2 characters)
-            // It takes from index 0 to (length - 2)
             allNames = allNames.substring(0, allNames.length() - 2);
 
             System.out.println("Hello, " + allNames + "!");
